9c4ae577ccd67b07da9b4e9e829b5bfc2de5a70b
MUH ALIM SOEDRAJAT <52270419+alimsan@users.noreply.github.com>
2025-05-02 23:45:54 +0800

    2/5/2025
    🔃tidak mau ribet karena untuk menghilangkan element membuat fungsi tidak bekerja , jadi hidden saja classnya untuk notes

--- resources/views/filament/resources/kasir-resource/pages/kasir-page.blade.php
+++ resources/views/filament/resources/kasir-resource/pages/kasir-page.blade.php
@@ -288,7 +288,7 @@
                         </div>
                     </div>
 
-                    <div>
+                    <div class="hidden">
                         <label for="notes" class="block text-sm font-medium text-gray-700 dark:text-gray-300">
                             Catatan
                         </label>
